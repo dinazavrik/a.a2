46c5a0c4670253023715b5dc8acb659168dbb8b6
Unknown <avedenko.a@mail.ru>
2017-11-03 12:14:00 +0200

    tz

---
+++ assets/src/js/scripts.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+    $(document).ready(function () {
+
+        $('.txt-slid__slider').slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        });
+
+    });
+
+})(jQuery);
+
+
+
